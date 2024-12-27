1a4b771e225802648c74a4e19eef899208b0e02d
Andrey Rybakov <rybakovwebdev@gmail.com>
2024-12-27 20:06:35 +0400

    Fix password reset route

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -6,21 +6,19 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
-  const code = searchParams.get("code");
 
-  const supabase = createClient();
+  if (token_hash && type) {
+    const supabase = createClient();
 
-  if (code) {
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
-    if (!error) {
-      return NextResponse.redirect(new URL("/account?reset=true", request.url));
-    }
-  } else if (token_hash && type) {
     const { error } = await supabase.auth.verifyOtp({
       type,
       token_hash,
     });
+
     if (!error) {
+      if (type === "recovery") {
+        return NextResponse.redirect(new URL("/account?reset=true", request.url));
+      }
       return NextResponse.redirect(new URL("/", request.url));
     }
   }
